e10e690c9af459273dc70f67c1791dd44d3f6dda
Khidagrim <tristanwaluigi@orange.fr>
2020-09-26 17:16:48 +0200

    recup data json

--- JudgeIt/Assets/Scripts/UI/CrimeHandler.cs
+++ JudgeIt/Assets/Scripts/UI/CrimeHandler.cs
@@ -1,11 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Linq;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+using System;
+
+[Serializable]
+public class ChargeData
+{
+    public int points ;
+    public string description;
+}
 
 public class CrimeHandler : MonoBehaviour
 {
+    public List<ChargeData> chargeDatas;
+
+    public Dictionary<string,string>[] dico;
+
+    private string chargefilename = "charge";
+
     public void Init()
     {
+        chargeDatas = new List<ChargeData>();
+        GetAllCharges();
+    }
+
+    private void GetAllCharges()
+    {
+        List<ChargeData> _charge = new List<ChargeData>();
+        string filePath = Path.Combine(Application.streamingAssetsPath, chargefilename + (chargefilename.EndsWith(".json") ? "" : ".json"));
+        string dataJsonAsString = File.ReadAllText(filePath);
+
+        Debug.Log(dataJsonAsString);
+ 
+        if (!string.IsNullOrEmpty(dataJsonAsString))
+        {
+            //var loadedData = JsonConvert.DeserializeObject<Dictionary<string, int>>(dataJsonAsString);
+            dico = JsonConvert.DeserializeObject<Dictionary<string,string>[]>(dataJsonAsString);
+        }
+
+        foreach(Dictionary<string,string> dic in dico)
+        {
+            ChargeData chargeData = new ChargeData();
+            
+            chargeData.description = dic["text"];
+            chargeData.points = int.Parse(dic["score"]);
+            chargeDatas.Add(chargeData);
+        }
 
     }
+
+
 }
